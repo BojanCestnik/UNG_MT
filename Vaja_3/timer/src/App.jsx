047950dbce1a57a4ff9d86e57e05fc1f7e18e896
Bojan <bojan.cestnik@temida.si>
2024-12-05 14:32:11 +0100

    updated timer jsx and renamed input to style

--- Vaja_3/timer/src/App.jsx
+++ Vaja_3/timer/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import TimerInput from './components/TimerInput';
 import TimerDisplay from './components/TimerDisplay';
 import TimerControls from './components/TimerControls';
@@ -42,26 +40,26 @@ function App() {
       setTimeLeft(0);
     };
 
-  return (
-    <>
-      <div className="card">
-        <h1>Timer</h1>
-        {/* <label for="duration">Enter time in seconds:</label><br></br>
-        <input type="number" id="duration" name="duration" min="1" max="1800"></input> */}
-        <TimerInput setDuration={setDuration} />
-
-        {/* <p>00</p> */}       
-        <TimerDisplay timeLeft={timeLeft} />
-
-        {/* <button id="start">start</button>
-        <button id="pause">pause</button>
-        <button id="reset">reset</button> */}
-        <TimerControls onStart={handleStart} onPause={handlePause} onReset={handleReset} />
-      </div>
-    </>
-  )
-}
-export default App 
+    return (
+      <>
+        <div className="card">
+          <h1>Timer</h1>
+          {/* <label for="duration">Enter time in seconds:</label><br></br>
+          <input type="number" id="duration" name="duration" min="1" max="1800"></input> */}
+          <TimerInput setDuration={setDuration} />
+  
+          {/* <p>00</p> */}       
+          <TimerDisplay timeLeft={timeLeft} />
+  
+          {/* <button id="start">start</button>
+          <button id="pause">pause</button>
+          <button id="reset">reset</button> */}
+          <TimerControls onStart={handleStart} onPause={handlePause} onReset={handleReset} />
+        </div>
+      </>
+    )
+  }
+  export default App
 
 
 /* Previous version: 
